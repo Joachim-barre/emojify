ea966ac04959681dcd011ef04da03637f4d9cf82
Joachim-barre <joachim.barre@laposte.net>
2023-12-14 18:13:54 +0100

    added encode handler

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import emojify
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for 
 
 app = Flask(__name__, template_folder="html")
 
@@ -7,5 +7,10 @@ app = Flask(__name__, template_folder="html")
 def home():
     return render_template("index.html")
 
+@app.route("/encode", methods=["POST"])
+def encoder(): 
+    pass
+
 if __name__ == "__main__":
+
     app.run()
