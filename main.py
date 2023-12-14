ecae867d099fc586d484018ea81f5e114d2cc49d
Joachim-barre <joachim.barre@laposte.net>
2023-12-14 16:17:39 +0100

    made emojify able to do it's work

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="html")
 
-@app.route("/") 
-def home(): 
-    return render_template("index.html")
+@app.route("/<text>") 
+def home(text): 
+    return render_template("index.html", result=emojify(text))
 
 if __name__ == "__main__":
     app.run()
