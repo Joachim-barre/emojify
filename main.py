84b341e16b144859114e7f1d62e15df06dcf55e3
Joachim-barre <joachim.barre@laposte.net>
2023-12-14 16:25:17 +0100

    removed old /<word> halder

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ app = Flask(__name__, template_folder="html")
 def home():
     return render_template("index.html")
 
-@app.route("/<text>") 
-def emojifier(text): 
-    return render_template("emojifier.html", result=emojify(text))
-
 if __name__ == "__main__":
     app.run()
