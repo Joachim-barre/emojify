b7c4f4841dbe5fd464dc313b8f23d1357c5b555c
Joachim-barre <joachim.barre@laposte.net>
2023-12-14 16:14:42 +0100

    corrected temlate folder used by the app

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from utils import emojify
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="html")
 
 @app.route("/") 
 def home(): 
