4743fcd9b1fcec641225891c025cb83228bd038e
Joachim-barre <joachim.barre@laposte.net>
2023-12-15 17:30:34 +0100

    created demojify method

--- utils.py
+++ utils.py
@@ -6,3 +6,12 @@ def emojify(text : str) -> str:
         else:
             output += chr(0x0001f500 + ord(i) )
     return output
+
+def demojify(text : str) -> str:
+    output : str = ""
+    for i in text:
+        if(i == '?' or hex(ord(i))[:-2] != "0x1f5"):
+            output += '?'
+        else:
+            output += chr(ord(i) - 0x0001f500)
+    return output
