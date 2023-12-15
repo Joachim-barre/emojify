641ccd3cdf6af7f0fdb9ff596ea4a8d3a8f10a0e
Joachim-barre <joachim.barre@laposte.net>
2023-12-15 17:30:43 +0100

    created demojify tests

--- tests/test.py
+++ tests/test.py
@@ -24,4 +24,25 @@ def test_emojify(argument : str, expected : str):
 def test_emojify_invalid(argument : str, expected : str):
     assert(utils.emojify(argument) == expected)
 
+@pytest.mark.parametrize(
+    "expected, argument",
+    [ 
+        ('a', '🕡'),
+        ('A', '🕁'),
+        ('abcdefgh', '🕡🕢🕣🕤🕥🕦🕧🕨')
+    ]
+)
+def test_demojify(argument : str, expected : str):
+    assert(utils.demojify(argument) == expected)
+
+@pytest.mark.parametrize(
+    "expected, argument",
+    [ 
+        ('?', '('),
+        ('A?', '🕁b'),
+        ('?b??ef??', 'a🕢?d🕥🕦£*')
+    ]
+)
+def test_demojify_invalid(argument : str, expected : str):
+    assert(utils.demojify(argument) == expected)
 
