1ce9e4f73b09224cfd0a26f01210271bbea4d966
Joachim-barre <joachim.barre@laposte.net>
2023-12-16 17:25:41 +0100

    updated js to support decoding

--- static/home.js
+++ static/home.js
@@ -11,10 +11,29 @@ document.getElementById("encode").addEventListener("submit", function (event) {
         .then(response => response.text())
         .then(data => {
         // Display the response on the page
-        document.getElementById("response").innerHTML = "Encoded: " + data;
+        document.getElementById("encoded").innerHTML = "Encoded: " + data;
     })
     .catch(error => {
     console.error("Error:", error);
     });
 });
 
+document.getElementById("decode").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var formData = new FormData(this);
+
+    // Use AJAX to send the form data to the server
+    fetch("/decode", {
+        method: "POST",
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+        // Display the response on the page
+        document.getElementById("decoded").innerHTML = "Decoded: " + data;
+    })
+    .catch(error => {
+    console.error("Error:", error);
+    });
+});
